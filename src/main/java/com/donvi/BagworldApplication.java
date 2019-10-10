62a4b81c549bdeda567dd75b385d2d72069aaab8
杨东伟 <ydw01@choicesoft.com.cn>
2019-10-10 14:55:35 +0800

    Initial commit

---
+++ src/main/java/com/donvi/BagworldApplication.java
@@ -0,0 +1,13 @@
+package com.donvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BagworldApplication.class, args);
+    }
+
+}
