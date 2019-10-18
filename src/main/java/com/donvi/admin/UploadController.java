7a17e9e821f1f6e0e8eaf9dab9cc28429c2b2c5c
杨东伟 <ydw01@choicesoft.com.cn>
2019-10-18 17:22:07 +0800

    图片上传

--- src/main/java/com/donvi/admin/UploadController.java
+++ src/main/java/com/donvi/admin/UploadController.java
@@ -1,9 +1,17 @@
 package com.donvi.admin;
 
 import com.donvi.utils.ServerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @Description:
@@ -14,10 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/upload")
 @RestController
 public class UploadController {
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
     @RequestMapping("/image")
     @ResponseBody
-    public ServerResponse uploadImage(){
+    public ServerResponse uploadImage(@RequestParam("testUploadImage") MultipartFile[] myfile, HttpServletRequest request) throws IOException {
+        String uploadPath = request.getSession().getServletContext().getRealPath("/img") + "/";
+        String fileName = myfile[0].getOriginalFilename();
+        String realFilePath = uploadPath + fileName;
+        logger.info("保存图片路径为：realFilePath = {}",realFilePath);
+        File uploadFile = new File(realFilePath);
+        myfile[0].transferTo(uploadFile);
         return null;
     }
 }
